d8a2e10d256bff769b14d2a1ecdb9fdff59fadc9
4z0t <gastec139@gmail.com>
2023-07-12 15:20:24 +0300

    cleanup terranunits.lua

--- hook/lua/terranunits.lua
+++ hook/lua/terranunits.lua
@@ -1,8 +1,8 @@
 do
-    oldTPodTowerUnit = TPodTowerUnit
-    TPodTowerUnit = Class(oldTPodTowerUnit) {
+    local TPodTowerUnitCreatePod = TPodTowerUnit.CreatePod
+    TPodTowerUnit = Class(TPodTowerUnit) {
         CreatePod = function(self, podName)
-            oldTPodTowerUnit.CreatePod(self, podName)
+            TPodTowerUnitCreatePod(self, podName)
             if self.LevelProgress and self.LevelProgress > 1.01 then
                 self.PodData[podName].PodHandle:AddLevels(self.LevelProgress - 1)
             end
